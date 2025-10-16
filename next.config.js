335f97501e5f355756014befd6f5f31d9302e6c3
samar220659 <a22061981@gmx.de>
2025-10-16 23:12:16 +0200

    feat: Add GitHub Actions deployment workflow

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-/** @type {import('next').NextConfig} */
+/** @.cache/node-gyp/24.9.0/include/node/cppgc/type-traits.h {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/LinktoFunnel' : '',
